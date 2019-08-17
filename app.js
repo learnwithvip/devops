43c532d45fe50137a27b961c64f43c9dd0056dc1
Ubuntu <ubuntu@ip-172-31-19-62.us-east-2.compute.internal>
2019-08-17 19:23:17 +0000

    add all js

---
+++ app.js
@@ -0,0 +1,11 @@
+ function getCookie()  
+    {  
+        if(document.cookie.length!=0)  
+        {  
+        alert(document.cookie);  
+        }  
+        else  
+        {  
+        alert("Cookie not available");  
+        }  
+    } 
